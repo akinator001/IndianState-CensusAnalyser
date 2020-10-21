aa7e94812555359c8fd9e4ba3f2da1f0d6583fe2
akinator001 <aakashgandhi1279@gmail.com>
2020-10-22 01:46:39 +0530

    [aakash] test to check file path

--- test/java/com/cp/indianstatecensusanalyser/StateCodeAnalyserTest.java
+++ test/java/com/cp/indianstatecensusanalyser/StateCodeAnalyserTest.java
@@ -7,7 +7,8 @@ import org.junit.Test;
 
 public class StateCodeAnalyserTest {
 	public static final String STATE_CODE_DATA = "StateCode.csv";
-
+	public static final String WRONG_STATE_CODE_DATA = "statecode2.csv";
+			
 	StateCodeAnalyser stateCodeAnalyser = new StateCodeAnalyser();
 
 	@Test
@@ -15,4 +16,13 @@ public class StateCodeAnalyserTest {
         int records = stateCodeAnalyser.loadCSVFile(Paths.get(STATE_CODE_DATA));
         Assert.assertEquals(37, records);
     }
+	@Test
+    public void checkWrongPath() throws CensusException{
+    	try {
+    		stateCodeAnalyser.loadCSVFile(Paths.get(WRONG_STATE_CODE_DATA));
+    	}
+    	catch(CensusException e) {
+    		Assert.assertEquals(CensusException.ExceptionType.WRONG_CSV, e.type);
+    	}
+    }	
 }
