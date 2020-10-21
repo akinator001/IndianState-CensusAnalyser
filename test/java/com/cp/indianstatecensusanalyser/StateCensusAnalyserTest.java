d801a5ef422e0b546092734dac9054d9c55ed316
akinator001 <aakashgandhi1279@gmail.com>
2020-10-22 01:11:43 +0530

    [aakash] test for number of entries in census file

--- test/java/com/cp/indianstatecensusanalyser/StateCensusAnalyserTest.java
+++ test/java/com/cp/indianstatecensusanalyser/StateCensusAnalyserTest.java
@@ -1,5 +1,17 @@
 package com.cp.indianstatecensusanalyser;
 
-public class StateCensusAnalyserTest {
+import java.nio.file.Paths;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+public class StateCensusAnalyserTest {
+	public static final String STATE_CENSUS_DATA = "./StateCensus.csv";
+
+    @Test
+    public void ensureNoOfRecordMatches(){
+        StateCensusAnalyser stateCensusAnalyser = new StateCensusAnalyser();
+        int records = stateCensusAnalyser.loadCSVFile(Paths.get(STATE_CENSUS_DATA));
+        Assert.assertEquals(29, records);
+    }
 }
