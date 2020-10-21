8fa7ee9cbfeedd77faf4e9a39d04fe42447be6dc
akinator001 <aakashgandhi1279@gmail.com>
2020-10-22 01:09:16 +0530

    [aakash] state census data loaded from file

--- main/java/com/cp/indianstatecensusanalyser/StateCensusAnalyser.java
+++ main/java/com/cp/indianstatecensusanalyser/StateCensusAnalyser.java
@@ -1,7 +1,29 @@
 package com.cp.indianstatecensusanalyser;
 
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+
 public class StateCensusAnalyser {
-	public static void main(String[] args) {
-		System.out.println("Welcome to Indian State Census Analyser Problem!!!");
+	public int loadCSVFile(Path path) {
+		try (Reader reader = Files.newBufferedReader(path)){
+			CsvToBean<StateCensus> csvToBean = new CsvToBeanBuilder(reader).withType(StateCensus.class).withIgnoreLeadingWhiteSpace(true).build();
+			Iterator<StateCensus> iterator = csvToBean.iterator();
+			ArrayList<StateCensus> stateCensusList = new ArrayList<StateCensus>();
+			while(iterator.hasNext()) {
+				 stateCensusList.add(iterator.next());
+			}
+			return stateCensusList.size();
+		}
+		catch(IOException e) {
+			e.printStackTrace();
+			return 0;
+		}	
 	}
 }
