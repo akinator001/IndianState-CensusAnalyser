4ff09594c7f8c04ecaa536c72c0ac5dbc1f784b6
akinator001 <aakashgandhi1279@gmail.com>
2020-10-22 01:48:51 +0530

    [aakash] test to check wrong header

--- test/java/com/cp/indianstatecensusanalyser/StateCodeAnalyserTest.java
+++ test/java/com/cp/indianstatecensusanalyser/StateCodeAnalyserTest.java
@@ -8,21 +8,35 @@ import org.junit.Test;
 public class StateCodeAnalyserTest {
 	public static final String STATE_CODE_DATA = "StateCode.csv";
 	public static final String WRONG_STATE_CODE_DATA = "statecode2.csv";
-			
-	StateCodeAnalyser stateCodeAnalyser = new StateCodeAnalyser();
-
+	public static final String WRONG_STATE_CODE_DATA_HEADER = "StateCensus.csv";
+		
+	
 	@Test
     public void ensureNoOfRecordMatches() throws CensusException{
-        int records = stateCodeAnalyser.loadCSVFile(Paths.get(STATE_CODE_DATA));
+		StateCodeAnalyser stateCodeAnalyser = new StateCodeAnalyser();
+		int records = stateCodeAnalyser.loadCSVFile(Paths.get(STATE_CODE_DATA));
         Assert.assertEquals(37, records);
     }
+	
 	@Test
     public void checkWrongPath() throws CensusException{
     	try {
+    		StateCodeAnalyser stateCodeAnalyser = new StateCodeAnalyser();
     		stateCodeAnalyser.loadCSVFile(Paths.get(WRONG_STATE_CODE_DATA));
     	}
     	catch(CensusException e) {
     		Assert.assertEquals(CensusException.ExceptionType.WRONG_CSV, e.type);
+    	}
+    }	
+	
+	@Test
+    public void checkWrongHeader() throws CensusException{
+    	try {
+    		StateCodeAnalyser stateCodeAnalyser = new StateCodeAnalyser();
+    		stateCodeAnalyser.loadCSVFile(Paths.get(WRONG_STATE_CODE_DATA_HEADER));
+    	}
+    	catch(CensusException e) {
+    		Assert.assertEquals(CensusException.ExceptionType.WRONG_HEADER, e.type);;
     	}
     }	
 }
